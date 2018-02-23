7a52487c50a90d58777887e8ec02afde8d933292
samssi <samir.hamadi@gmail.com>
2018-02-23 22:46:31 +0200

    Change foreground color too

--- 0-change-background-color/bgcolor.asm
+++ 0-change-background-color/bgcolor.asm
@@ -2,6 +2,8 @@
     org $1000 ; Memory location for the program to start in Hex. (in decimal 4096) This is where SYS instruction should be pointed
 
     ldx #$04 ; Store hex-value 04 to register x as a byte
+    ldy #$01 ;
 main:
-    stx $d020 ; Move x register byte to memory background color memory location.
+    stx $d020 ; Move x register byte to memory background color memory location. 04 corresponds color purple
+    sty $d021 ; Move y register byte to memory foreground color location. 01 corresponds to white
     jmp main ; Loop the program
