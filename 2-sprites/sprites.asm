ac863e170eb19d853b54ed9fa9f3ee642ac1c21c
samssi <samir.hamadi@gmail.com>
2018-02-25 19:07:02 +0200

    Make use of all registers

--- 2-sprites/sprites.asm
+++ 2-sprites/sprites.asm
@@ -3,12 +3,12 @@
 
     lda #$80
     sta $d07f8 ; Memory location of the sprite
-    lda #$01
-    sta $d015 ; Which sprites are enabled. eg. 01 -> 0000001, 02 -> 0000010, 03 -> 0000011. 01 Sprite 1 enable, 10 Sprite 2 enabled, 11 Sprite 1 and 2 enabled (think in switches)
+    ldx #$01
+    stx $d015 ; Which sprites are enabled. eg. 01 -> 0000001, 02 -> 0000010, 03 -> 0000011. 01 Sprite 1 enable, 10 Sprite 2 enabled, 11 Sprite 1 and 2 enabled (think in switches)
 
-    lda #$80 ; Decimal value of 128. Location on screen X or Y. Sprite X and Y values are the same and here
-    sta $d000 ; Sprite 1, X
-    sta $d001 ; Sprite 1, Y
+    ldy #$80 ; Decimal value of 128. Location on screen X or Y. Sprite X and Y values are the same and here
+    sty $d000 ; Sprite 1, X
+    sty $d001 ; Sprite 1, Y
 
 loop:
     jmp loop
